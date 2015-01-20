c51f3300f456b66df8cfc5a5eefeb4c4bd0ff5d8
evaillant <emmanuel.v@me.com>
2015-01-20 11:25:39 +0100

    travis

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-console.log("Hello World!");
-console.log("What's up?");
-console.log("Ok :)");
+var express = require('express');
+
+// Constants
+var PORT = 9000;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
